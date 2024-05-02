537802882a3a21bc437bc90b6fccdaae3dceec77
ds-pweick <ds-pweick@users.noreply.github.com>
2024-05-02 06:26:31 +0000

    chore(docs): Update docs for release 5.0.17

--- irs-api/src/main/java/org/eclipse/tractusx/irs/IrsApplication.java
+++ irs-api/src/main/java/org/eclipse/tractusx/irs/IrsApplication.java
@@ -59,7 +59,7 @@ public class IrsApplication {
     /**
      * The IRS API version.
      */
-    public static final String API_VERSION = "5.0.16";
+    public static final String API_VERSION = "5.0.17";
 
     /**
      * The URL prefix for IRS API URLs.
