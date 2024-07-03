14537aa5d129f399e7fb30c037d6663e5dad4910
ds-pweick <162321742+ds-pweick@users.noreply.github.com>
2024-07-03 11:12:18 +0000

    chore(docs): updated docs and Helm chart for IRS release 5.3.3

--- irs-api/src/main/java/org/eclipse/tractusx/irs/IrsApplication.java
+++ irs-api/src/main/java/org/eclipse/tractusx/irs/IrsApplication.java
@@ -59,7 +59,7 @@ public class IrsApplication {
     /**
      * The IRS API version.
      */
-    public static final String API_VERSION = "5.3.2";
+    public static final String API_VERSION = "5.3.3";
 
     /**
      * The URL prefix for IRS API URLs.
